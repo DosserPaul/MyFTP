e969d1e83a3c75ea76c6c4fabbb242c4629e7ec0
Dosser <paul.dosser@epitech.eu>
2022-04-15 11:32:23 +0200

    ADD[src/]: i forgot one file

---
+++ src/check_error.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** MyFTP
+** File description:
+** check_error
+*/
+
+#include "../includes/myftp.h"
+
+int check_error(int nb, const char *str)
+{
+    if (nb == -1) {
+        perror(str);
+        exit(84);
+    }
+    return nb;
+}
