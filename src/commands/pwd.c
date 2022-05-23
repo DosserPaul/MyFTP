8482fd41eac11d5cfa97cf158db2b5e760a7e30c
Dosser <paul.dosser@epitech.eu>
2022-05-23 21:40:15 +0200

    feat[commands]: add some check is_loggef in functions

--- src/commands/pwd.c
+++ src/commands/pwd.c
@@ -9,8 +9,8 @@
 
 client_t *pwd(client_t *client, char **cmd UNUSED)
 {
-//    if (is_logged(client) == false)
-//        return client;
+    if (is_logged(client) == false)
+        return client;
 
     char *path = getcwd(NULL, 0);
 
@@ -20,9 +20,6 @@ client_t *pwd(client_t *client, char **cmd UNUSED)
     int len = (int)strlen(client->path);
     if (len != 1 && client->path[len - 1] == '/')
         client->path[strlen(client->path) - 1] = '\0';
-//
-//    if (len == 0)
-//        client->path = strdup("/");
 
     dprintf(client->fd, "257 \"%s\"\r\n", client->path);
     free(path);
